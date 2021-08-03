a7fd904ebc3284b9cd52562d947cf5a67e963211
ThinkPad <zzz100826@gmail.com>
2021-08-03 13:43:05 +0800

    add: ex.35 - ex.37

--- chapter2/Section1.java
+++ chapter2/Section1.java
@@ -97,6 +97,101 @@ public class Section1 {
         StdOut.printf("new seq: %d ms\n", t3 - t2);
     }
 
+    public static void exercise35() {
+        int length = 1000000;
+        Double[] uniform = new Double[length];
+        for (int i = 0; i < uniform.length; ++i) {
+            uniform[i] = StdRandom.uniform();
+        }
+        Double[] guass = new Double[length];
+        for (int i = 0; i < guass.length; ++i) {
+            guass[i] = StdRandom.gaussian();
+        }
+        Integer[] poisson = new Integer[length];
+        for (int i = 0; i < poisson.length; ++i) {
+            poisson[i] = StdRandom.poisson(0.5);
+        }
+        Integer[] geometric = new Integer[length];
+        for (int i = 0; i < geometric.length; ++i) {
+            geometric[i] = StdRandom.geometric(0.5);
+        }
+        Integer[] discrete = new Integer[length];
+        for (int i = 0; i < discrete.length; ++i) {
+            discrete[i] = StdRandom.discrete(StdRandom.permutation(1000));
+        }
+        long t1 = System.currentTimeMillis();
+        Shell.sort(uniform); // 1519
+        long t2 = System.currentTimeMillis();
+        Shell.sort(guass); // 1095
+        long t3 = System.currentTimeMillis();
+        Shell.sort(poisson); // 154
+        long t4 = System.currentTimeMillis();
+        Shell.sort(geometric); // 83
+        long t5 = System.currentTimeMillis();
+        Shell.sort(discrete); // 666
+        long t6 = System.currentTimeMillis();
+        StdOut.println(t2 - t1);
+        StdOut.println(t3 - t2);
+        StdOut.println(t4 - t3);
+        StdOut.println(t5 - t4);
+        StdOut.println(t6 - t5);
+    }
+
+    public static void exercise36() {
+        int length = 1000000;
+        Integer[] bits = new Integer[length];
+        for (int i = 0; i < bits.length; ++i) {
+            bits[i] = StdRandom.bernoulli() ? 0 : 1;
+        }
+        Integer[] incre = new Integer[length];
+        for (int i = 0; i < incre.length; ++i) {
+            incre[i] = StdRandom.bernoulli() ? 0 : StdRandom.bernoulli() ? 1 : 2;
+        }
+        Integer[] halfRandom = new Integer[length];
+        for (int i = 0; i < halfRandom.length; ++i) {
+            halfRandom[i] = StdRandom.bernoulli() ? 0 : StdRandom.uniform(length);
+        }
+        long t1 = System.currentTimeMillis();
+        Shell.sort(bits); // 145
+        long t2 = System.currentTimeMillis();
+        Shell.sort(incre); // 97
+        long t3 = System.currentTimeMillis();
+        Shell.sort(halfRandom); // 345
+        long t4 = System.currentTimeMillis();
+        StdOut.println(t2 - t1);
+        StdOut.println(t3 - t2);
+        StdOut.println(t4 - t3);
+    }
+
+    public static void exercise37() {
+        int length = 1000000, border = (int) 0.95 * length;
+        Integer[] randomValue = new Integer[length];
+        for (int i = 0; i < border; ++i) {
+            randomValue[i] = i;
+        }
+        for (int i = border; i < randomValue.length; ++i) {
+            randomValue[i] = StdRandom.uniform(length);
+        }
+        Integer[] closeToPos = new Integer[length];
+        for (int i = 0; i < closeToPos.length; ++i) {
+            closeToPos[i] = i - StdRandom.uniform(10);
+        }
+        Integer[] randomPosition = new Integer[length];
+        for (int i = 0; i < randomPosition.length; ++i) {
+            randomPosition[i] = StdRandom.bernoulli(0.95) ? i : StdRandom.uniform(length);
+        }
+        long t1 = System.currentTimeMillis();
+        Shell.sort(randomValue); // 1323
+        long t2 = System.currentTimeMillis();
+        Shell.sort(closeToPos); // 113
+        long t3 = System.currentTimeMillis();
+        Shell.sort(randomPosition); // 533
+        long t4 = System.currentTimeMillis();
+        StdOut.println(t2 - t1);
+        StdOut.println(t3 - t2);
+        StdOut.println(t4 - t3);
+    }
+
     public static void main(String[] args) {
     }
 
