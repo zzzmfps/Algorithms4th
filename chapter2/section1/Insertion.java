c452e622125e6a1a7a7171bc90567965b84c64b0
ThinkPad <zzz100826@gmail.com>
2021-08-09 17:06:39 +0800

    update: add indirectSort for Section5

--- chapter2/section1/Insertion.java
+++ chapter2/section1/Insertion.java
@@ -47,4 +47,21 @@ public class Insertion extends AbstractSort {
         return index;
     }
 
+    public static <T extends Comparable<T>> int[] indirectSort(T[] array) {
+        int[] index = new int[array.length];
+        index[0] = Insertion.minIndex(array, 0, array.length);
+        for (int i = 1; i < index.length; ++i) {
+            index[i] = i;
+        }
+        index[index[0]] = 0;
+        for (int i = 1, j; i < array.length; ++i) {
+            int k = index[i];
+            for (j = i; AbstractSort.less(array[k], array[index[j - 1]]); j -= 1) {
+                index[j] = index[j - 1]; // no exch
+            }
+            index[j] = k;
+        }
+        return index;
+    }
+
 }
